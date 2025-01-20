0f9fc4169400565706f24ffa094de91cbc0c7411
Andreas Evans-Adamecz <andreas.evansadamecz@dcmail.ca>
2025-01-20 17:44:53 -0500

    set up basic structure

---
+++ src/client/app/index.js
@@ -0,0 +1,7 @@
+/* 
+Name: Andreas Evans-Adamecz
+filename: index.js
+Course: INFT 2202
+Date: January 14, 2025
+Description: general application file
+*/
---
+++ src/client/app/product.mock.service.js
@@ -0,0 +1,7 @@
+/* 
+Name: Andreas Evans-Adamecz
+filename: product.mock.service.js
+Course: INFT 2202
+Date: January 14, 2025
+Description: general application file
+*/
