7ba367c5fa7f7e0df3655820c6a39ee3be55c2b0
Andreas Evans-Adamecz <andreas.evansadamecz@dcmail.ca>
2025-02-01 12:02:10 -0500

    finished setting up the html files, Product class, and have begun working on the mock service class

--- src/client/app/Product.js
+++ src/client/app/Product.js
@@ -3,5 +3,28 @@ Name: Andreas Evans-Adamecz
 filename: Product.js
 Course: INFT 2202
 Date: January 14, 2025
-Description: general application file
+Description: Product class file
 */
+
+export default class Product {
+    constructor({id = null, name, price, stock, description, imageURL}) {
+        this.id = id ?? crupto.randomUUID();
+
+        Object.assign(this, {name, price, stock, description, imageURL})
+    }
+
+    toString() {
+        return `${this.name} costs $${this.price.toFixed(2)}, has ${this.stock} in stock, and is described as: "${this.description}".`; 
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            price: this.price,
+            stock: this.stock,
+            description: this.description,
+            imageURL: this.imageURL
+        };
+    }
+}
--- src/client/app/product.mock.service.js
+++ src/client/app/product.mock.service.js
@@ -3,5 +3,34 @@ Name: Andreas Evans-Adamecz
 filename: product.mock.service.js
 Course: INFT 2202
 Date: January 14, 2025
-Description: general application file
+Description: Mock service file for managing product data
 */
+
+import Product from "./Product.js";
+
+export default new ProductService();
+
+function ProductService() {
+    if(!localStorage.getItem('products')) {
+        localStorage.setItem('products', JSON.stringify([]));
+    }
+}
+
+ProductService.prototype.listProducts = function (page = 1, perPage = 5) {
+    const first = (page -1) * perPage;
+    const last = first + perPage;
+
+    const products = JSON.parse(localStorage.getItem("products"))
+        .map(productParams => new Product(productParams))
+        .slice(first, last);
+    
+    return products;
+};
+
+ProductService.prototype.getAllProducts = function() {
+    return JSON.parse(localStorage.getItem("products")).map(product => new Product(product));
+};
+
+ProductService.listProductCount = function() {
+    return JSON.parse(localStorage.getItem("products")).length;
+};
